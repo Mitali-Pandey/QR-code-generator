2915b9f5c65903e5f22574314136e4fa68cb132b
Mitali Pandey <mitalipandey498@gmail.com>
2025-07-24 21:10:11 +0530

    QR-code generator

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 import inquirer from 'inquirer';
 import qr from 'qr-image'
-import fs from 'fs'
+import fs from 'fs';
 
 inquirer
   .prompt([
     /* Pass your questions in here */
     {
-        message: "write your URL : ",
-        name : "URL"
+      message: "write your URL : ",
+      name: "URL"
     }
   ])
   .then((answers) => {
     const url = answers.URL;
 
     var qr_svg = qr.image(url);
-qr_svg.pipe(require('fs').createWriteStream('qr_img.png'));
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+
+    fs.writeFile("URL.txt", url, (err) =>{
+      if(err) throw err;
+      console.log("the file has been saved!");
+    })
+
   })
   .catch((error) => {
     if (error.isTtyError) {
